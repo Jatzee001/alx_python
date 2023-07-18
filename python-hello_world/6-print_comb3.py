ea0d442eddd07c7e48c241e4e2a1bb2072b3129f
Jatzee001 <jatto_i@yahoo.com>
2023-07-18 10:09:33 -0700

    inventing is a combination

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
-for i in range(10):
+for i in range(1, 10):
     for j in range(i + 1, 10):
-        print("{:02d}".format(i), end=", " if j != 9 else "{:02d}".format(j))
+        print("{:d}{:d}".format(i, j), end=", " if i < 8 else "\n")
