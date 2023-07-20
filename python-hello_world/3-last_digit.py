e20162a35094b90d6dfd62bd795289da9e8aa8da
Jatzee001 <jatto_i@yahoo.com>
2023-07-20 02:08:07 -0700

    last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -14,5 +14,4 @@ elif last_digit == 0:
     string = "0"
 else:
     string = "less than 6 and not 0"
-
-print("Last digit of {} is {} and is {}".format(number, last_digit, string))
+print("last digit of {} is {} and is ()". format(number, last_digit, string))
