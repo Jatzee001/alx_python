21bfb4fbdc14c8d4184815b0e6dd7253fed251a6
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-07-28 13:26:18 +0100

    Can you C me now

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,22 +1,9 @@
 def no_c(my_string):
-    new_string = ""
+    
+    str = " "
     for char in my_string:
         if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
-
-# Test cases
-test_cases = [
-    "School",
-    "Chicago",
-    "Holberton",
-    "Holberton School",
-    "",
-    "HellcCcccooccoscccss",
-]
-
-for word in test_cases:
-    print(f"Correct output - case: word = \"{word}\"")
-    result = no_c(word)
-    print(result)
-    print()
+            str += char
+    return str
+element = no_c("school")
+print(element)
