dc6e202483fb67e3cb061b6ccb68f51cae8894fb
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-07-28 12:48:15 +0100

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,15 +3,9 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        return None
+        result = None
     finally:
-        print("Inside result: {}".format(result))
-
-    return result
-
-# Test the function
+        if result is not None:
+            print("Inside result: {}".format(result))
+        return result
 result1 = safe_print_division(10, 2)
-result2 = safe_print_division(5, 0)
-
-print("Returned result1:", result1)
-print("Returned result2:", result2)
