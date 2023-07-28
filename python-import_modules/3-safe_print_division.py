e6c76541a2223322418685a6481e24cde3637e34
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-07-28 12:39:57 +0100

    Integers division with debug

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,15 +2,24 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        return None
+        result = None
     finally:
         print("Inside result: {}".format(result))
-
+    
     return result
 
 # Test the function
+print("Correct output - case: a = 10 / b = 2")
 result1 = safe_print_division(10, 2)
-result2 = safe_print_division(5, 0)
 
-print("Returned result1:", result1)
-print("Returned result2:", result2)
+print("Correct output - case: a = 10 / b = -2")
+result2 = safe_print_division(10, -2)
+
+print("Correct output - case: a = 0 / b = 2")
+result3 = safe_print_division(0, 2)
+
+print("Correct output - case: a = 10 / b = 0")
+result4 = safe_print_division(10, 0)
+
+print("Correct output - case: a = 0 / b = 0")
+result5 = safe_print_division(0, 0)
