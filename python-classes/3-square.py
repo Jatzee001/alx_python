3b612069b27f259eaf1ff5cc63af1d11ac453f88
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-07-30 15:54:32 +0100

    Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,6 +12,19 @@ Example:
 """
 
 class Square:
+    """A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a square object with an optional size.
+        size(self): Getter method to retrieve the size of the square.
+        size(self, value): Setter method to set the size of the square.
+        area(self): Calculates and returns the area of the square.
+    """
+
+
     def __init__(self, size=0):
         self.__size = size
 
