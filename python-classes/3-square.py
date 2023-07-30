7a7b04cf8dadab7bf81549ae7bac68575534325d
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-07-30 15:51:05 +0100

    Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,9 +1,30 @@
+"""This module defines a Square class.
+
+The Square class represents a square with a private instance attribute 'size'.
+It allows instantiation with an optional size, and provides a method 'area' to calculate the square's area.
+The size attribute can be accessed using a property 'size' with a setter for validation.
+
+Example:
+    # Create a square object with size 5
+    square = Square(5)
+    print(square.size)  # Output: 5
+    print(square.area())  # Output: 25 (5 * 5)
+"""
+
 class Square:
     def __init__(self, size=0):
         self.__size = size
 
     @property
     def size(self):
+        """Get or set the size of the square.
+
+        The size must be an integer, otherwise a TypeError is raised.
+        If the size is less than 0, a ValueError is raised.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.__size
 
     @size.setter
@@ -16,4 +37,9 @@ class Square:
             self.__size = value
 
     def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
