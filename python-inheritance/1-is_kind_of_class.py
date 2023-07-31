ac0504383dd496005b7a49b9fc675044cc3a0cc3
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-07-31 12:54:48 +0100

    Same class or inherit from

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -12,3 +12,29 @@ def is_kind_of_class(obj, a_class):
         the specified class; otherwise False.
     """
     return isinstance(obj, a_class)
+
+
+# Test cases
+a = 1
+print(is_kind_of_class(a, int))  # Output: True
+
+a = True
+print(is_kind_of_class(a, int))  # Output: False
+
+a = 3.14
+print(is_kind_of_class(a, int))  # Output: False
+
+a = True
+print(is_kind_of_class(a, object))  # Output: True
+
+a = None
+print(is_kind_of_class(a, object))  # Output: True
+
+a = None
+print(is_kind_of_class(a, list))  # Output: False
+
+a = [1, 2, 3]
+print(is_kind_of_class(a, list))  # Output: True
+
+a = [1, 2, 3]
+print(is_kind_of_class(a, object))  # Output: True
