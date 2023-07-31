18920825830e04253154b7a41e81d560b3c56a95
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-01 00:32:55 +0100

    Full rectangle

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,8 +1,46 @@
 class BaseGeometry:
-    # ... (same as before)
+    """
+    Base class for geometry.
+
+    This class provides a base for other geometry classes to inherit from.
+
+    Methods:
+        area(self): Raises an Exception with the message "area() is not implemented".
+        integer_validator(self, name, value): Validates the integer value.
+
+    Attributes:
+        name: The name of the value being validated.
+        value: The integer value to be validated.
+    """
+
+    def area(self):
+        """Calculate the area of the geometry.
+
+        Raises:
+            Exception: Always raises an Exception with the message "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate the integer value.
+
+        Args:
+            name (str): The name of the value being validated.
+            value: The integer value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
 
 class Rectangle(BaseGeometry):
-    """A class representing a rectangle.
+    """
+    A class representing a rectangle.
 
     This class inherits from BaseGeometry and adds functionality specific to rectangles.
 
@@ -35,36 +73,25 @@ class Rectangle(BaseGeometry):
 
 
 # Test cases
-r = Rectangle(5, 10)
-
-# Output: [Rectangle] 5/10
-print(str(r))
-
-# Output: 50 (5 * 10)
-print(r.area())
-
-# Output: List of attributes and methods of the Rectangle class
 print(dir(Rectangle))
 
-# Output: True (Rectangle is a subclass of BaseGeometry)
-print(issubclass(Rectangle, BaseGeometry))
-
-# Output: No exception is raised
 r = Rectangle(1, 4)
+print(r.area())  # Output: 4
 
-# Output: Raises ValueError: "width" must be greater than 0
-r = Rectangle(0, 4)
+r = Rectangle(1411, 781)
+print(r.area())  # Output: 1103191
 
-# Output: Raises TypeError: "height" must be an integer
-r = Rectangle(3, "3")
+r = Rectangle(5, 5)
+print(r.area())  # Output: 25
 
-# Output: 3
-r = Rectangle(3, 5)
-print(r.width)
+r = Rectangle(1411, 781)
+print(r)  # Output: [Rectangle] 1411/781
 
-# Output: 5
-r = Rectangle(3, 5)
-print(r.height)
+r = Rectangle(1, 4)
+print(r)  # Output: [Rectangle] 1/4
 
-# Output: Raises TypeError: __init__() missing 2 required positional arguments: 'width' and 'height'
+r = Rectangle(5, 5)
+print(r)  # Output: [Rectangle] 5/5
+
 r = Rectangle()
+# Output: TypeError: __init__() missing 2 required positional arguments: 'width' and 'height'
