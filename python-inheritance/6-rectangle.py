87faf25d6b7687c4953aee63ff2f2da2c3e9ae6b
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-03 17:21:49 +0100

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,40 +1,38 @@
+#!/usr/bin/python3
+"""6-rectangle.py"""
+
+
 class BaseGeometry:
-    """Base class for geometry.
-
-    This class provides a base for other geometry classes to inherit from.
-
-    """
+    """BaseGeometry class"""
 
     def area(self):
-        """Calculate the area of the geometry.
-
-        Raises:
-            Exception: Always raises an Exception with the message "area() is not implemented".
-        """
+        """Raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """Validates value
+        Args:
+            name (str): name of the parameter
+            value (int): value to validate
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less or equal to 0
         """
-        Validate the integer value.
-
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """A class representing a rectangle.
+    """Rectangle class that inherits from BaseGeometry"""
 
-    This class inherits from BaseGeometry and adds functionality specific to rectangles.
-
-    Attributes:
-        __width (int): The width of the rectangle.
-        __height (int): The height of the rectangle.
-    """
-
-    def __init__(self, width, height):
+    def _init_(self, width, height):
+        """Instantiates a Rectangle object
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
