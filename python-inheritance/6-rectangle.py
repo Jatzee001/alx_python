a1be271b00bbd9b9aa70fa5738e043b08aa72d95
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-04 11:38:28 +0100

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
 """6-rectangle.py"""
-
-
 class BaseGeometry:
-    """BaseGeometry class"""
-
     def area(self):
-        """Raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value
-        Args:
-            name (str): name of the parameter
-            value (int): value to validate
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less or equal to 0
-        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
@@ -25,15 +12,34 @@ class BaseGeometry:
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry"""
-
-    def _init_(self, width, height):
-        """Instantiates a Rectangle object
-        Args:
-            width (int): width of the rectangle
-            height (int): height of the rectangle
-        """
+    def __init__(self, width, height):
+        super().__init__()
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+
+# Test cases
+r = Rectangle(3, 5)
+print(dir(r))
+print(issubclass(Rectangle, BaseGeometry))
+
+try:
+    r = Rectangle(0, 4)
+except ValueError as e:
+    print(e)  # Output: width must be greater than 0
+
+try:
+    r = Rectangle(3, "3")
+except TypeError as e:
+    print(e)  # Output: height must be an integer
+
+r = Rectangle(3, 5)
+print(r._Rectangle__width)  # Output: 3
+print(r._Rectangle__height)  # Output: 5
+
+try:
+    r = Rectangle()
+except TypeError as e:
+    print(e)  # Output: __init__() missing 2 required positional arguments: 'width' and 'height'
