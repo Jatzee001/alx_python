9ba09473be5c2a72b928e327d0f77654af239feb
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-07 17:13:31 +0100

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,18 +1,25 @@
 """Represents a new rectangle"""
 
-class Base:
-    def __init__(self, width, height, id=None):
-        """
-        Base class constructor.
+def __init__(self, width, height, id=None):
+        """Initialize a new Rectangle.
 
         Args:
-            width (int): The width of the shape.
-            height (int): The height of the shape.
-            id (int, optional): The identifier for the shape. Defaults to None.
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
         """
         self.__width = width
         self.__height = height
-        self.__id = id
+        self.x = x
+        self.y = y
+        super().__init__(id)
 
     def area(self):
         """
