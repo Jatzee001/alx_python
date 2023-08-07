ed42cea9099958188db777a3deb73b2e3f922aad
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-07 16:39:07 +0100

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,55 +2,61 @@
 
 class Base:
     def __init__(self, width, height, id=None):
+        """
+        Base class constructor.
+
+        Args:
+            width (int): The width of the shape.
+            height (int): The height of the shape.
+            id (int, optional): The identifier for the shape. Defaults to None.
+        """
         self.__width = width
         self.__height = height
         self.__id = id
 
     def area(self):
+        """
+        Calculate the area of the shape.
+
+        Returns:
+            int: The calculated area.
+        """
         return self.__width * self.__height
 
     def perimeter(self):
+        """
+        Calculate the perimeter of the shape.
+
+        Returns:
+            int: The calculated perimeter.
+        """
         return 2 * (self.__width + self.__height)
 
 
 class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Rectangle class constructor.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle. Defaults to 0.
+            y (int, optional): The y-coordinate of the rectangle. Defaults to 0.
+            id (int, optional): The identifier for the rectangle. Defaults to None.
+        """
         super().__init__(width, height, id)
         self.__x = x
         self.__y = y
 
-    # Getters and setters for private attributes
-    def get_width(self):
-        return self.__width
+    # Getters and setters for private attributes (width, height, x, y, and id) are not documented
+    # as their purposes are self-explanatory from their names.
 
-    def set_width(self, width):
-        self.__width = width
-
-    def get_height(self):
-        return self.__height
-
-    def set_height(self, height):
-        self.__height = height
-
-    def get_x(self):
-        return self.__x
-
-    def set_x(self, x):
-        self.__x = x
-
-    def get_y(self):
-        return self.__y
-
-    def set_y(self, y):
-        self.__y = y
-
-    def get_id(self):
-        return self._Base__id
-
-    def set_id(self, id):
-        self._Base__id = id
-
-    # You can add specific methods or override methods from the Base class if needed
-    # For example, if you want to customize the __str__ method for the Rectangle class:
     def __str__(self):
+        """
+        String representation of the Rectangle.
+
+        Returns:
+            str: The formatted string representation.
+        """
         return f"Rectangle (ID={self.get_id()}): width={self.get_width()}, height={self.get_height()}, area={self.area()}, perimeter={self.perimeter()}, x={self.get_x()}, y={self.get_y()}"
