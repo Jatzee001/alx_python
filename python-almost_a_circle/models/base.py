976b92f63efd48e2bc1cac573a61f884dfa11a25
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-07 13:33:10 +0100

    Base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,3 +7,21 @@ class ClassBase:
         else:
             ClassBase.__nb_objects += 1
             self.id = ClassBase.__nb_objects
+
+# Test the ClassBase functionality
+if __name__ == "__main__":
+    # Create an instance without passing an id (should assign id=1)
+    obj1 = ClassBase()
+    print(obj1.id)  # Output: 1
+
+    # Create another instance without passing an id (should assign id=2)
+    obj2 = ClassBase()
+    print(obj2.id)  # Output: 2
+
+    # Create an instance passing an id (should assign id=10)
+    obj3 = ClassBase(10)
+    print(obj3.id)  # Output: 10
+
+    # Create another instance without passing an id (should assign id=3, which is 1 + 1)
+    obj4 = ClassBase()
+    print(obj4.id)  # Output: 3
