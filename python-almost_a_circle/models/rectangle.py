1a201f1eea5678ef0e622c4b96266b18a0f3de74
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-07 18:22:54 +0100

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -26,4 +26,42 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         super().__init__(id)
-        
+
+    @property
+    def width(self):
+        """Set the width of the Rectangle."""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set the height of the rectangle"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Set the x coordinate of the Rectangle."""
+        return self.__x
+    
+    @x.setter
+    def x(self,value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("height must be > 0")
+        self.__x = value
