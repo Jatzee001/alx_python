7d412c27d56fb20d5db75dfa70fd6f6c25a47438
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-10 16:51:18 +0100

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 
-"""Defines a square class"""
+"""
+Defines a square class
+"""
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """Represent a square."""
 
-    def __init__(self, size, x=0, id=None):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
+    def __init__(self, size, x=0, y=0, id=None):
         """
-        super().__init__(size, size, x, y, id)
+        Initialize a new Square.
+        """
+        super().__init__(size, size, x=0, y=0, id=None)
+
+    def __str__(self):
+        """
+        str doc
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
         """Set/get the size of the Square."""
-        return self.__width
+        return self.width
     
     @size.setter
     def size(self, value):
+        """
+        size setter method
+        """
+
         self.width = value
         self.height = value
