979b3511093c28e488d282c7f8d8c981bed6bcd5
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-11 13:43:33 +0100

    POST an email #1

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+import requests
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: {} <URL> <email>".format(sys.argv[0]))
+    sys.exit(1)
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = {'email': email}
+response = requests.post(url, data=data)
+
+print(response.text)
