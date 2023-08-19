4118f8c7e0d59d11e8783382e56967d94477ca97
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-19 13:08:36 +0100

    display new

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -106,10 +106,12 @@ class Rectangle(Base):
 
     def display(self):
         """
-        Print the Rectangle instance using the character '#'.
+        Print the Rectangle instance using the character '#' while taking into account x and y positions.
         """
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """
@@ -121,8 +123,10 @@ class Rectangle(Base):
         return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
 
 if __name__ == "__main__":
-    r1 = Rectangle(4, 6, 2, 1, 12)
-    print(r1)
+    r1 = Rectangle(2, 3, 2, 2)
+    r1.display()
 
-    r2 = Rectangle(5, 5, 1)
-    print(r2)
+    print("---")
+
+    r2 = Rectangle(3, 2, 1, 0)
+    r2.display()
