90a2cd5ef9be1a515c4c70a9e365b251d68d1aff
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-19 13:31:01 +0100

    square size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -12,6 +12,8 @@ class Square(Rectangle):
     Public Methods:
         - __init__(self, size, x=0, y=0, id=None): Initializes a Square instance.
         - __str__(self): Returns the string representation of the Square instance.
+        - size (property): Getter for the size attribute.
+        - size (setter): Setter for the size attribute.
     """
 
     def __init__(self, size, x=0, y=0, id=None):
@@ -26,31 +28,35 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
+    @property
+    def size(self):
         """
-        Return a string representation of the square.
+        Getter for the size attribute.
 
         Returns:
-            str: The square description in the format [Square] (<id>) <x>/<y> - <size>.
+            int: The size of the square.
         """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter for the size attribute.
+
+        Args:
+            value (int): The new size value to set.
+        """
+        self.width = value
+        self.height = value
 
 if __name__ == "__main__":
     s1 = Square(5)
     print(s1)
-    print(s1.area())
-    s1.display()
+    print(s1.size)
+    s1.size = 10
+    print(s1)
 
-    print("---")
-
-    s2 = Square(2, 2)
-    print(s2)
-    print(s2.area())
-    s2.display()
-
-    print("---")
-
-    s3 = Square(3, 1, 3)
-    print(s3)
-    print(s3.area())
-    s3.display()
+    try:
+        s1.size = "9"
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
