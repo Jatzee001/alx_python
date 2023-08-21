b681aea75ac310ec61b6fac1f7b00c745f961ddb
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-21 12:58:10 +0100

    display

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -112,16 +112,6 @@ class Rectangle(Base):
             int: The area of the rectangle.
         """
         return self.__width * self.__height
-    
-if __name__ == "__main__":
-    r1 = Rectangle(3, 2)
-    print(r1.area())
-
-    r2 = Rectangle(2, 10)
-    print(r2.area())
-
-    r3 = Rectangle(8, 7, 0, 0, 12)
-    print(r3.area())
 
     def display(self):
         """
