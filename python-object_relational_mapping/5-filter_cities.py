f1a27eb9d6353d70e4667836008c1b771d48d0d8
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-08-31 11:39:01 +0100

    5. All cities by state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,39 @@
+"""
+first task in Python ORM
+"""
+
+import MySQLdb
+import sys
+
+user = sys.argv[1]
+passwd = sys.argv[2]
+database = sys.argv[3]
+state = sys.argv[4]
+
+db = MySQLdb.connect(user=user, passwd=passwd, database=database)
+cur = db.cursor()
+
+
+def main():
+    """
+    the main function
+    """
+    query = """
+    SELECT name
+    FROM cities
+    WHERE state_id = (
+        SELECT id
+        FROM states
+        WHERE name = %s
+        );
+    """
+    cur.execute(query, (state,))
+    cities = cur.fetchall()
+    for index, city in enumerate(cities):
+        print(city[0], end=", " if index < len(cities) - 1 else "")
+    else:
+        print()
+
+
+if __name__ == "__main__":
+    main()
