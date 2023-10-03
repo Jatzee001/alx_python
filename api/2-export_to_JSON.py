2f84f2a09ab32b004d9c97cad0ec7d0eb45b914a
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-10-03 16:31:11 +0100

    json

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,15 +1,25 @@
 """
 This script fetches employee data and exports it in JSON format.
-It records all tasks owned by the specified employee.
+It records all tasks owned by the specified employee and stores them in a JSON file.
 
 Usage: python export_to_JSON.py <employee_id>
 """
 
+import csv
+import json
 import requests
 import sys
-import json
 
 def get_employee_data(employee_id):
+    """
+    Fetches employee data and exports it in JSON format.
+
+    Args:
+        employee_id (int): The ID of the employee for whom data is fetched.
+
+    Returns:
+        None
+    """
     # Define the base URL for the API
     base_url = "https://jsonplaceholder.typicode.com"
 
@@ -27,17 +37,20 @@ def get_employee_data(employee_id):
         print(f"No tasks found for Employee {username} (ID: {user_id}).")
         return
 
-    # Create a dictionary to store the tasks in JSON format
-    task_dict = {str(user_id): []}
+    # Create a list to store the tasks as dictionaries
+    tasks_list = []
     for task in todos_data:
         task_completed = task["completed"]
         task_title = task["title"]
-        task_dict[str(user_id)].append({"task": task_title, "completed": task_completed, "username": username})
+        tasks_list.append({"task": task_title, "completed": task_completed, "username": username})
+
+    # Create a dictionary with user ID as the key and list of tasks as the value
+    user_tasks_dict = {str(user_id): tasks_list}
 
     # Create a JSON file for the employee
     json_filename = f"{user_id}.json"
     with open(json_filename, 'w') as json_file:
-        json.dump(task_dict, json_file, indent=4)
+        json.dump(user_tasks_dict, json_file, indent=4)
 
     print(f"Data exported to {json_filename}")
 
