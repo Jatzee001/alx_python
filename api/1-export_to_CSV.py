1e93a248e7f44f4f8655a2d3e3bb9784f1c0011e
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-10-03 16:02:09 +0100

    Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,45 @@
+import requests
+import sys
+import csv
+
+def get_employee_data(employee_id):
+    # Define the base URL for the API
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Make a request to get employee details
+    employee_response = requests.get(f"{base_url}/users/{employee_id}")
+    employee_data = employee_response.json()
+    user_id = employee_data.get("id")
+    username = employee_data.get("username")
+
+    # Make a request to get the TODO list for the employee
+    todos_response = requests.get(f"{base_url}/users/{employee_id}/todos")
+    todos_data = todos_response.json()
+
+    # Create a CSV file for the employee
+    csv_filename = f"{user_id}.csv"
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        
+        # Write the header row
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        # Write each task to the CSV file
+        for task in todos_data:
+            task_completed = "True" if task["completed"] else "False"
+            csv_writer.writerow([user_id, username, task_completed, task["title"]])
+
+    print(f"Data exported to {csv_filename}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python export_to_CSV.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    get_employee_data(employee_id)
